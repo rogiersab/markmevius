339aef9914516c6bb21f05bbd9ec748d72ccacd0
rogiersab <94569380+rogiersab@users.noreply.github.com>
2026-02-25 13:57:57 +0100

    Fix next.config.js to use proper Next.js configuration format

--- next.config.js
+++ next.config.js
@@ -1,11 +1,3 @@
-'use strict';
-
-const { nextConfig } = require('next/config');
-
 module.exports = {
   reactStrictMode: true,
-  images: {
-    domains: ['example.com'], // Add your image domains here
-  },
-  // Other Next.js configurations go here
 };
